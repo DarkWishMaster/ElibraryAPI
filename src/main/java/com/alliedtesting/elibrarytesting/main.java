6028eda20f6b0dc7836d69cca385a370e380619c
Vladislav Cebotari <cebotari.vladislav@gmail.com>
2014-11-19 22:24:15 +0200

    Finished some small tests for the API. Will start to implement API functions.

--- src/main/java/com/alliedtesting/elibrarytesting/main.java
+++ src/main/java/com/alliedtesting/elibrarytesting/main.java
@@ -1,5 +1,11 @@
 package com.alliedtesting.elibrarytesting;
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -22,7 +28,7 @@ import com.google.gson.GsonBuilder;
  */
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		/*
 		System.setProperty("webdriver.chrome.driver", "D:/Software/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -43,7 +49,36 @@ public class main {
 		System.out.println("Author DOB  = " +  a.getDateOfBirth());
 		String json2 = gson.toJson(a);
 		System.out.println(json2);
-
+		
+		/* GET-  http://{host}/elibraryws/books/{bookId} */
+		String url = "http://81.180.75.144:8080/elibraryws/books/1";
+		 
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(url);
+ 
+ 
+		HttpResponse response = client.execute(request);
+ 
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + 
+                       response.getStatusLine().getStatusCode());
+ 
+		BufferedReader rd = new BufferedReader(
+                       new InputStreamReader(response.getEntity().getContent()));
+ 
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+ 
+		
+		System.out.println(result);
+		/* Cheers, all works fine */
+/*
+ * 	TODO:
+ * 		- modify classes for http client to handle exceptions
+ */
 	}
 		
 	
